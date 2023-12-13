8f7f40caf286d392f1fb09e02da29a88dafa3400
Karthik Ch <saipavan39dh@gmail.com>
2023-12-13 21:46:46 +0530

    Reverse Display of past sessions

--- src/components/pages/PastSessions.js
+++ src/components/pages/PastSessions.js
@@ -122,7 +122,7 @@ const PastSessions = () => {
                  />
                </div>
              ) : null;
-           })
+           }).reverse()
          ) : (
            <Loading />
          )}
