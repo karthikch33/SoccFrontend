9714c44070cda67d501cdec47e24b09b547d0c0f
Karthik Ch <saipavan39dh@gmail.com>
2023-12-16 21:50:38 +0530

    server bulb attached

--- src/components/pages/Bulb.js
+++ src/components/pages/Bulb.js
@@ -1,31 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { TbHandFinger } from "react-icons/tb";
+import axios from 'axios'
+import { GetSessions } from '../features/session/sessionSlice';
 
 const Bulb = () => {
   const [isOn, setIsOn] = useState(false);
-  const checkBulbStatus = async () => {
-    try {
-      const response = await fetch('https://soccbackend.onrender.com');
-      const data = await response.json();
-      localStorage.setItem('server',JSON.stringify({'server':'ON'}))
-      setIsOn(true);
-    } catch (error) {
-      console.error('Error checking bulb status:', error);
-    }
-  };
-
-  useEffect(() => {
-    checkBulbStatus();
-    const intervalId = setInterval(checkBulbStatus, 60000);
-    return () => clearInterval(intervalId);
-  }, []);
-
   return (
     <div className="container-xxl text-center">
         <div className="row">
             <div className="col-12 d-flex justify-content-end " style={{background:"#f8f9fa"}}>
                 <label htmlFor="">Make Sure Server Is On<TbHandFinger className='fs-3' style={{rotate:"90deg"}}/></label>
-             <div className={`bulb ${isOn ? 'on' : 'off'}`} ></div>
+             <div className={`bulb ${localStorage.getItem('server')? 'on' : 'off'}`} ></div>
              </div>
         </div>
              <div className="row">
