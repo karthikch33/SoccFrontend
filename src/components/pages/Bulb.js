b7712d2c52383aa19a60cb94267623780df93ed3
Karthik Ch <saipavan39dh@gmail.com>
2023-12-24 11:06:45 +0530

    changed this

--- src/components/pages/Bulb.js
+++ src/components/pages/Bulb.js
@@ -21,6 +21,19 @@ const Bulb = () => {
   
   return (
     <>
+    <div className="container-xxl text-center">
+        <div className="row">
+            <div className="col-12 d-flex justify-content-end " style={{background:"#f8f9fa"}}>
+                <label htmlFor="">Make Sure Server Is On<TbHandFinger className='fs-3' style={{rotate:"90deg"}}/></label>
+                {serverCondition?.status === 201?<div className={`bulb on`} ></div>:<div className={`bulb off`} ></div>}
+             </div>
+        </div>
+             <div className="row">
+                <div className="col-12 d-flex justify-content-end">
+             <label htmlFor="" className='' style={{fontSize:"10px"}}>(Reload For Confirmation)</label>
+                </div>
+            </div>
+    </div>
     </>
   );
 };
