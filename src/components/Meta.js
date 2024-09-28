8267a6502386f354ec73c27e1b0b097a3cd38152
Karthik Ch <saipavan39dh@gmail.com>
2024-09-28 10:25:26 +0530

    socc logo updated

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -4,9 +4,9 @@ import {Helmet} from 'react-helmet'
 const Meta = (props) => {
     const {title} = props;
   return (
-    <Helmet>
+    <Helmet>  
     {/* <meta charSet="utf-8" /> */}
-    <link rel="icon" type="image/png" href="https://media.licdn.com/dms/image/D4E03AQH8PEP16-aAbA/profile-displayphoto-shrink_400_400/0/1663862227248?e=1707350400&v=beta&t=4Ae96Qem5yl0LRJvX4OHpN1C1a5H3EIcHHtL2Ny33Jo" />
+    <link rel="icon" type="image/png" href="https://res.cloudinary.com/ddkdeeue3/image/upload/v1727499284/jm97kywmlw1o3vjbgmux.jpg" />
     <title>{title}</title>
 </Helmet>
   )
