0e325d82b85b5faf28619f444f30679a5f50d956
Karthik Ch <saipavan39dh@gmail.com>
2025-03-23 12:38:42 +0530

    Last Commit by Hp Victuz

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -41,7 +41,7 @@ const Layout = () => {
       <div className='dark jumbo relative flex flex-col h-[100vh] items-center justify-center bg-white dark:bg-transparent transition-bg'>
         <div className="absolute inset-0 overflow-hidden">
           <div className="jumbo absolute -inset-[10px] opacity-50"></div>
-        </div>
+        </div> 
         <div className="mt-4">
         <Bulb/>
             <Outlet/>
--- src/components/utils/base_url.js
+++ src/components/utils/base_url.js
@@ -1,4 +1,4 @@
-const url = 'https://soccbackend.onrender.com/api/'
-// const url = 'http://localhost:1507/api/'
+// const url = 'https://soccbackend.onrender.com/api/'
+const url = 'http://localhost:1507/api/'
 // const url = ''
 export default url
